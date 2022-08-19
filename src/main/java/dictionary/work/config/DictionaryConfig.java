e90ae3594a94df8fd19b6dccba1b9d286c3d8476
BROLLER99 <boris-ato@mail.ru>
2022-08-19 13:57:39 +0300

    Renamed string constants in the DictionaryConfig class

--- src/main/java/dictionary/work/config/DictionaryConfig.java
+++ src/main/java/dictionary/work/config/DictionaryConfig.java
@@ -8,8 +8,8 @@ import dictionary.work.DAO.RunTimeDictionary;
  * Класс определяет тип хранилища для словаря
  */
 public class DictionaryConfig {
-    private static final int ONE_FOR_COMMAND_LINE = 1;
-    private static final int ZERO_FOR_COMMAND_LINE = 0;
+    private static final int MORE_THAN_ZERO_ARGUMENTS_ON_THE_COMMAND_LINE = 1;
+    private static final int ZERO_ARGUMENTS_ON_THE_COMMAND_LINE=0;
 
     /**
      * Метод определяет тип хранения словаря в зависимости от наличия аргументов командной строки
@@ -17,17 +17,17 @@ public class DictionaryConfig {
      * @return Создает локальный словарь, если есть аргументы командной строки и runtime словарь, если их нет
      */
     public static Dictionary createDictionary(String[] args) {
-        if (parsCommandLineArgs(args) == ONE_FOR_COMMAND_LINE) {
+        if (parsCommandLineArgs(args) == MORE_THAN_ZERO_ARGUMENTS_ON_THE_COMMAND_LINE) {
             return new LocalDictionary();
         } else {
             return new RunTimeDictionary();
         }
     }
     private static int parsCommandLineArgs(String[] args) {
-        if (args.length > ZERO_FOR_COMMAND_LINE) {
-            return ONE_FOR_COMMAND_LINE;
+        if (args.length > ZERO_ARGUMENTS_ON_THE_COMMAND_LINE) {
+            return MORE_THAN_ZERO_ARGUMENTS_ON_THE_COMMAND_LINE;
         } else {
-            return ZERO_FOR_COMMAND_LINE;
+            return ZERO_ARGUMENTS_ON_THE_COMMAND_LINE;
         }
     }
 }
